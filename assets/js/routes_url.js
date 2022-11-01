2683e721f52f4291e116cf031f5727a9ce8fd74a
iamklaus <iamklaus01@gmail.com>
2022-11-02 00:16:40 +0100

    Feat : New URL for adding integer constraints

--- assets/js/routes_url.js
+++ assets/js/routes_url.js
@@ -1,5 +1,20 @@
-const REGISTER_URL = "http://localhost:8000/auth/register";
-const LOGIN_URL = "http://localhost:8000/auth/login";
+export const REGISTER_URL = "http://localhost:8000/auth/register";
+export const LOGIN_URL = "http://localhost:8000/auth/login";
+export const LOGOUT_URL = "http://localhost:8000/auth/logout";
+export const DELETE_ACCOUNT_URL = "http://localhost:8000/auth/delete";
+export const PROFILE_URL = "http://localhost:8000/auth/profile";
+export const GET_ALL_USERS_URL = "http://localhost:8000/admin/all_users";
+export const GET_USER_FILES_URL = "http://localhost:8000/auth/files";
 
+export const STATS_URL = "http://localhost:8000/admin/stats";
 
-export {REGISTER_URL, LOGIN_URL}
+//Admin URL
+export const CREATE_ADMIN_URL = "http://localhost:8000/admin/user/register";
+
+//Make selection operation url
+export const UPLOAD_FILE_URL = "http://localhost:8000/candidates/files/add"
+export const SAVE_FEATURES_URL = "http://localhost:8000/candidates/files/add_features"
+export const GET_DETAILS_INT_FEATURES_URL = "http://localhost:8000/candidates/features/int/details"
+export const GET_DETAILS_ENUM_FEATURES_URL = "http://localhost:8000/candidates/features/enum/details"
+export const SAVE_INT_CONSTRAINTS_URL = "http://localhost:8000/candidates/constraints/iconstraints/add"
+export const SAVE_ENUM_CONSTRAINTS_URL = "http://localhost:8000/candidates/constraints/econstraints/add"
