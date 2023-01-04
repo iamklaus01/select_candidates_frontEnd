829c1bc34ce3f3aeaf794ddcbfc52a073667df6b
iamklaus <iamklaus01@gmail.com>
2023-01-04 10:08:50 +0100

    Feat : New method added for managing get in touch with admin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+import * as user from './api_bridge.js'
+
 let header = document.querySelector("header");
 let step_list = document.querySelector('.title-timeline ul');
 
@@ -14,4 +16,28 @@ window.addEventListener('scroll',() => {
 
 function isHidden(el) {
     return (el.offsetParent === null)
+}
+
+document.getElementById('get_in_touch').addEventListener('submit', get_in_touch);
+
+
+async function get_in_touch(e) {
+    e.preventDefault();
+    try {
+        let name = document.getElementsByName('name')[0].value;
+        let email = document.getElementsByName('email')[0].value;
+        let message = document.getElementsByName('message')[0].value;
+        let response = await user.get_in_touch(name, email, message);
+        if(response.ok){
+            let data = response.json();
+            data.then((message) =>{
+                console.log(message)
+            })
+        }else{
+            console.log('Error....... Email address may not be correct')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
